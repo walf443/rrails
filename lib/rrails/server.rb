fcf670402ff11e6758242f8164e2a9e766c2656b
Ryuta Kamizono <kamipo@gmail.com>
2012-09-27 00:37:51 +0900

    remove_connection is not necessary. included in establish_connection.

--- lib/rrails/server.rb
+++ lib/rrails/server.rb
@@ -74,7 +74,6 @@ module RemoteRails
     def dispatch(sock, line)
       args = Shellwords.shellsplit(line)
       subcmd = args.shift
-      ActiveRecord::Base.remove_connection if defined?(ActiveRecord::Base)
       servsock_out, clisock_out = UNIXSocket.pair
       servsock_err, clisock_err = UNIXSocket.pair
       pid = fork do
